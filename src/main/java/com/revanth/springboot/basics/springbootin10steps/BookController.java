7b9bc8defd01d0eceb956883f6d688e71b9347ed
revanth <revanthchimmani@gmail.com>
2020-03-02 00:33:12 +0530

    added BookController

---
+++ src/main/java/com/revanth/springboot/basics/springbootin10steps/BookController.java
@@ -0,0 +1,15 @@
+package com.revanth.springboot.basics.springbootin10steps;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+	@GetMapping("/books")
+	public List<Book> getAllBooks() {
+		return Arrays.asList(new Book(1, "Java", "Revanth"));
+	}
+}
